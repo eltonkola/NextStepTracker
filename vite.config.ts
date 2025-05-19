c10b0e4f6113920d62a05c85ab28a3b48e44a7b0
Elton Kola <eltonkola@gmail.com>
2025-05-19 13:38:16 -0400

    sfsdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,12 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
-    plugins: [
-      react(),
-      ghPages({
-        branch: 'gh-pages',
-        repo: 'https://github.com/eltonkola/NextStepTracker.git'
-      })
-    ],
+    plugins: [react()],
     optimizeDeps: {
       exclude: ['lucide-react'],
     },
